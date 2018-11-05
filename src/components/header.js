36a6ae8f54d1ec57253c9aad39fe539d1bd3b063
Sam Dalton <spd4nf@virginia.edu>
2018-11-05 14:03:17 -0600

    added info section

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import TopNav from './top-nav';
+import Info from './info'
 
 export default function Header(props){
   return(
     <header>
       <TopNav />
       <h1>Hot or Cold</h1>
+      <Info />
     </header>
   );
 }
